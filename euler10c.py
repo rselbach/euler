767f504bf92108ad493b3bd54ae206af212e1a67
Roberto Teixeira <robteix@robteix.com>
2010-08-06 01:17:54 +0000

    euler10c.py added solving problem #10 using Sieve of Eratosthenes. Small fixes on previous implementations.

---
+++ euler10c.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+def eratos(limit):  
+	if limit < 2:
+		return []  
+	sieve_size = limit / 2
+	sieve = [True] * sieve_size
+
+	for i in range(0, int(limit ** 0.5) / 2):  
+		if not sieve[i]:
+			continue  
+		for j in range((i * (i + 3) * 2) + 3, sieve_size, (i * 2) + 3):  
+			sieve[j] = False
+
+	primes = [2]  
+	primes.extend([(i * 2) + 3 for i in range(0, sieve_size) if sieve[i]])  
+
+	return primes
+
+print reduce(lambda x,y: x+y, eratos(2000000))
