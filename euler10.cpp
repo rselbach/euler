42d671bc2932bab7ebcc4898c1dc8cd42bacc387
Roberto Teixeira <robteix@robteix.com>
2010-08-06 14:05:38 +0000

    Euler #10 using boost::dynamic_bitset

--- euler10.cpp
+++ euler10.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
-#include <bitset>
-
+#include <boost/dynamic_bitset.hpp>
 using namespace std;
+
+#define LIMIT 2000000
 
 int main()
 {
-	bitset < 1000000 > Sieve;
-	unsigned long sum = 0;
+	boost::dynamic_bitset<> sieve(LIMIT);
+	unsigned long long sum = 0;
 
-	Sieve.flip();		// Set all bits to 1 
-	Sieve[0].flip();	// Set 0 and 1 to not prime 
-	Sieve[1].flip();
+	sieve.flip();		// Set all bits to 1 
+	sieve[0].flip();	// Set 0 and 1 to not prime 
+	sieve[1].flip();
 
 	// Check all nos from 2 to 1 million 
-	for (long i = 2; i < 1000000; ++i) {
-		if (!Sieve[i])	// If marked not prime 
+	for (unsigned long i = 2; i < LIMIT; ++i) {
+		if (!sieve[i])	// If marked not prime 
 			continue;	// return to head of loop 
 		else
 			// Set all multiples as not prime 
-			for (long j = 2 * i; j < 1000000; j += i)
-				Sieve[j] = 0;
+			for (unsigned long j = 2 * i; j < LIMIT; j += i)
+				sieve[j] = 0;
 	}
 
-	for (long i = 2; i < 1000000; ++i)
-		if (Sieve[i])	// Add all nos with bit set 
+	for (unsigned long i = 0; i < LIMIT; ++i)
+		if (sieve[i]){	// Add all nos with bit set
 			sum += i;
+		}
 
 	cout << "\nThe sum is : " << sum << endl;
 
